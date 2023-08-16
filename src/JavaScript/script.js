e587c4fb9b7151d1e57043fe4d587b32dcc55a89
apaolaoliveira <paolaoliveira.dev@gmail.com>
2023-08-16 04:20:50 -0300

    first commit

---
+++ src/JavaScript/script.js
@@ -0,0 +1,32 @@
+const value = document.getElementById('value');
+const plusButton = document.getElementById('plus');
+const minusButton = document.getElementById('minus');
+const resetButton = document.getElementById('reset');
+
+const UpdateValue = () => {
+    value.innerHTML = count;
+};
+
+let count = 0;
+let intervalId = 0;
+
+plusButton.addEventListener('mousedown', () => {
+    intervalId = setInterval(() => {
+        count += 1;
+        UpdateValue();
+    }, 100);
+});
+
+minusButton.addEventListener('mousedown', () => {
+    intervalId = setInterval(() => {
+        count -= 1;
+        UpdateValue();
+    }, 100);
+});
+
+resetButton.addEventListener('click', () => {
+    count = 0;
+    UpdateValue();
+});
+
+document.addEventListener('mouseup', () => clearInterval(intervalId));
